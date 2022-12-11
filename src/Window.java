11ac91d5b85fe15a9a28650cc3ba7f3082d7c868
lipeeeee <filipe.pereirafga@gmail.com>
2022-12-11 02:09:27 +0000

    Already feel my brain expanding, optimizing window drawing

--- src/Window.java
+++ src/Window.java
@@ -1,17 +1,5 @@
 import java.awt.*;
-import java.awt.font.FontRenderContext;
-import java.awt.font.GlyphVector;
-import java.awt.geom.AffineTransform;
 import java.awt.geom.Line2D;
-import java.awt.geom.Point2D;
-import java.awt.geom.Rectangle2D;
-import java.awt.image.BufferedImage;
-import java.awt.image.BufferedImageOp;
-import java.awt.image.ImageObserver;
-import java.awt.image.RenderedImage;
-import java.awt.image.renderable.RenderableImage;
-import java.text.AttributedCharacterIterator;
-import java.util.Map;
 import javax.swing.*;
 
 public class Window extends JPanel {
@@ -30,26 +18,29 @@ public class Window extends JPanel {
         this.window = new JFrame();
         this.window.setTitle(this.title);
         this.window.setSize(this.WIDTH, this.HEIGHT);
-        this.window.getContentPane().setPreferredSize(new Dimension(this.WIDTH, this.HEIGHT));
-        this.window.getContentPane().setBackground(this.backgroundColor);
         this.window.setResizable(this.resizable);
-        this.window.setLocationRelativeTo((Component)null);
+        this.window.setLocationRelativeTo(null);
         this.window.setVisible(true);
+        this.window.setContentPane(this);
+        this.window.getContentPane().setBackground(this.backgroundColor);
 
         // close on application close
         this.window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // optimization: drawing grid here saves a call to paintComponent()
+        drawGrid(this.window.getGraphics());
     }
 
+    // this should only be called when AI/User input for optimization purposes
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        drawGrid(g);
     }
 
     // draws tic-tac-toe 3x3 grid
     private void drawGrid(Graphics g){
         Graphics2D graphics2D = (Graphics2D) g;
-
-        for (int i = 0; i < 3; i++){
+        System.out.println("grid");
+        for (int i = 0; i < 10; i++){
             Line2D line = new Line2D.Float(i * 100, i * 100, i * 200, i * 200);
             graphics2D.draw(line);
         }
