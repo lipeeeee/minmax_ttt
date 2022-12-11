4e1a98c2f52094c560189428a6fbc3a088625ccf
lipeeeee <filipe.pereirafga@gmail.com>
2022-12-11 01:52:40 +0000

    learning JFrame

--- src/Window.java
+++ src/Window.java
@@ -1,33 +1,58 @@
 import java.awt.*;
+import java.awt.font.FontRenderContext;
+import java.awt.font.GlyphVector;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Line2D;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
+import java.awt.image.BufferedImageOp;
+import java.awt.image.ImageObserver;
+import java.awt.image.RenderedImage;
+import java.awt.image.renderable.RenderableImage;
+import java.text.AttributedCharacterIterator;
+import java.util.Map;
 import javax.swing.*;
 
-public class Window {
-    final int WIDTH = 500;
-    final int HEIGHT = 500;
-    final String title = "MinMax - TicTacToe";
-    final boolean resizable = false;
-    final Color backgroundColor = Color.BLACK;
+public class Window extends JPanel {
+    private JFrame window;
+    private final int WIDTH = 500;
+    private final int HEIGHT = 500;
+    private final String title = "MinMax - TicTacToe";
+    private final boolean resizable = false;
+    private final Color backgroundColor = Color.BLACK;
 
     public Window() {
         createWindow();
     }
 
     private void createWindow(){
-        JFrame window = new JFrame();
-        window.setSize(this.WIDTH, this.HEIGHT);
-        window.setTitle(this.title);
-        window.getContentPane().setBackground(this.backgroundColor);
-        window.setResizable(this.resizable);
-        window.setLocationRelativeTo((Component)null);
-        window.setVisible(true);
+        this.window = new JFrame();
+        this.window.setTitle(this.title);
+        this.window.setSize(this.WIDTH, this.HEIGHT);
+        this.window.getContentPane().setPreferredSize(new Dimension(this.WIDTH, this.HEIGHT));
+        this.window.getContentPane().setBackground(this.backgroundColor);
+        this.window.setResizable(this.resizable);
+        this.window.setLocationRelativeTo((Component)null);
+        this.window.setVisible(true);
 
         // close on application close
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        drawGrid(g);
     }
 
     // draws tic-tac-toe 3x3 grid
-    private void drawGrid(){
-        // TODO
+    private void drawGrid(Graphics g){
+        Graphics2D graphics2D = (Graphics2D) g;
+
+        for (int i = 0; i < 3; i++){
+            Line2D line = new Line2D.Float(i * 100, i * 100, i * 200, i * 200);
+            graphics2D.draw(line);
+        }
     }
 
     public int getHEIGHT() {
@@ -48,5 +73,9 @@ public class Window {
 
     public Color getBackgroundColor() {
         return backgroundColor;
+    }
+
+    public JFrame getWindow() {
+        return window;
     }
 }
